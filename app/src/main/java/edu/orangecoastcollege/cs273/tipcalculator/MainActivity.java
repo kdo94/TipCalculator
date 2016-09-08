cea65d9882c432f8c852728581ca9a9bcb01947e
kdo94 <kdo94@student.cccd.edu>
2016-09-08 16:31:00 -0700

    Finished the UI and connected the view to the controller. Implemented class RestaurantBill class.

--- app/src/main/java/edu/orangecoastcollege/cs273/tipcalculator/MainActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/tipcalculator/MainActivity.java
@@ -1,13 +1,67 @@
 package edu.orangecoastcollege.cs273.tipcalculator;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Associate controller with needed views
+    private EditText amountEditText;
+    private TextView amountTextView;
+    private TextView percentTextView;
+    private TextView tipTextView;
+    private TextView taxTextView;
+    private TextView totalTextView;
+    private SeekBar percentSeekBar;
+
+    // Associate controller with the needed model
+    RestaurantBill currentBill = new RestaurantBill();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Connect the controller with the widgets in our app
+        amountEditText = (EditText) findViewById(R.id.amountEditText);
+        amountTextView = (TextView) findViewById(R.id.amountTextView);
+        percentTextView = (TextView) findViewById(R.id.percentTextView);
+        tipTextView = (TextView) findViewById(R.id.tipTextView);
+        taxTextView = (TextView) findViewById(R.id.taxTextView);
+        totalTextView = (TextView) findViewById(R.id.totalTextView);
+        percentSeekBar = (SeekBar) findViewById(R.id.percentSeekBar);
+
+        // Define a listener for the amountEditText ( onTextChanged )
+        amountEditText.addTextChangedListener(amountTextChangedListener);
     }
+
+    private TextWatcher amountTextChangedListener = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            // Do nothing
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            // Try to get the amount from amountEditText
+            try{
+                double amount = Double.parseDouble(charSequence.toString()) / 100;
+                currentBill.setAmount(amount);
+            }
+            catch (NumberFormatException e){
+                amountEditText.setText("");
+            }
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+            // Do nothing
+        }
+    };
+
 }
---
+++ app/src/main/java/edu/orangecoastcollege/cs273/tipcalculator/RestaurantBill.java
@@ -0,0 +1,56 @@
+package edu.orangecoastcollege.cs273.tipcalculator;
+
+/**
+ * Created by kdo94 on 9/8/2016.
+ */
+public class RestaurantBill {
+    private double mAmount;
+    private double mTipPercent;
+    private double mTipAmount;
+    private double mTotalAmount;
+
+    public RestaurantBill(){
+        mAmount = 0.0;
+        mTipAmount = 0.0;
+        mTipPercent = 0.0;
+        mTotalAmount = 0.0;
+    }
+
+    public RestaurantBill(double mTotalAmount, double mTipAmount) {
+        this.mTotalAmount = mTotalAmount;
+        this.mTipAmount = mTipAmount;
+        recalculateAmount();
+    }
+
+    public double getAmount() {
+        return mAmount;
+    }
+
+    public void setAmount(double mAmount) {
+        this.mAmount = mAmount;
+        recalculateAmount();
+    }
+
+    public double getTipPercent() {
+        return mTipPercent;
+    }
+
+    public void setTipPercent(double mTipPercent) {
+        this.mTipPercent = mTipPercent;
+        recalculateAmount();
+    }
+
+    public double getTipAmount() {
+        return mTipAmount;
+    }
+
+
+    public double getTotalAmount() {
+        return mTotalAmount;
+    }
+
+    private void recalculateAmount(){
+        mTipAmount = mTotalAmount * mTipPercent;
+        mTotalAmount = mAmount + mTipPercent;
+    }
+}
